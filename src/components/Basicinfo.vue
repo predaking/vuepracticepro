ff944a33987226417e565cf8d2c48c7f26832c5a
Tratune <604181883@qq.com>
2018-12-10 18:22:29 +0800

    Js

--- src/components/Basicinfo.vue
+++ src/components/Basicinfo.vue
@@ -1,15 +1,46 @@
 <template>
-<div>
-
-</div>
+	<div class="PathInner" id="PathMenu" style=" margin:250px auto;">
+		<div class="PathMain">
+			<div class="Tmain" @click="tc()">
+				<div class="rotate"><span class="cover"></span></div>
+			</div>
+		</div>
+		<div class="PathItem">
+			<a class="link" href="#" title="更多图片"> 
+				<span class="item" style="background-image:url(../assets/Navigation/moment_icn_pic.png);"></span>
+			</a>
+		</div>
+		<div class="PathItem"> 
+			<a class="link" href="#" title="详细参数"> 
+				<span class="item" style="background-image:url(../assets/Navigation/moment_icn_info.png);"></span> 
+			</a>
+		</div>
+		<div class="PathItem"> 
+			<a class="link" href="#" title="玛百莉官方专卖店地址"> 
+				<span class="item" style="background-image: url(../assets/Navigation/moment_icn_address.png);"></span> 
+			</a>
+		</div>
+		<div class="PathItem"> 
+			<a class="link" href="#"> 
+				<span class="item" style="background-image: url(../assets/Navigation/moment_icn_price.png);"></span> 
+			</a>
+		</div>
+	</div>
 </template>
+
 <script>
+import Navi from "../js/lanrenzhijia";
 export default {
   name: 'Basicinfo',
   data() {
     return {
       
     }
+  },
+  methods:{
+  	tc(){
+  		Navi.PathRun();
+  	}
   }
 }
 </script>
@@ -18,5 +49,131 @@ export default {
   background-color: powderblue;
   text-align: center;
   padding: 5px;
+}
+.PathMenu {
+	position:absolute;
+	right: 0px;
+	bottom: 0px;
+	width: 60px;
+	height: 60px;
+}
+.PathInner {
+	position:relative;
+	width: 60px;
+	height: 60px;
+}
+.PathInner .PathItem {
+	position:absolute;
+	left: 0px;
+	bottom: 0px;
+}
+.PathInner a {/* position:absolute; */
+	display:block;/* overflow:hidden; */
+	background-position:center;
+	background-repeat:no-repeat;
+	z-index:999;
+}
+.PathInner a .item, .rotate {
+	width:100%;
+	height:100%;
+	background-position:center;
+	background-repeat:no-repeat;
+	display:block;
+	overflow:hidden;
+	text-align:center;
+	vertical-align:middle;
+	position:absolute;
+}
+.PathInner .PathMain {
+	z-index:1000;
+	position:absolute;
+	display:block;/* overflow:hidden; */
+	background-position:center;
+	background-repeat:no-repeat;
+	bottom:0;
+	left:0;
+}
+.PathInner .PathMain .Tmain {
+	background-image: url(../assets/Navigation/bg-2x.png);
+	width: 60px;
+	height: 60px;
+}
+.PathInner .PathMain .Tmain .rotate {
+	background-image: url(../assets/Navigation/icon-2x.png);
+	-moz-transform: rotate(0deg);
+	position:absolute;
+	bottom:0;
+	left:0;
+}
+.PathInner .cover {
+	width:100%;
+	height:100%;
+	display:block;
+	background:url(../assets/Navigation/bg-hl-2x.png) center no-repeat;
+	cursor: pointer;
+*filter:alpha(opacity=0);
+	filter:alpha(opacity=0)\0;
+	opacity:0;
+-webkit-transition:opacity .2s ease-out;
+-moz-transition:opacity .2s ease-out;
+-ms-transition:opacity .2s ease-out;
+-o-transition:opacity .2s ease-out;
+}
+.PathInner .cover:hover {
+	opacity:0.2;
+*filter:alpha(opacity=20);
+	filter:alpha(opacity=20)\0;
+}
+.PathItem .link {
+	position:absolute;
+	bottom:0;
+	left:0;
+	width: 60px;
+	height: 60px;
+	background-image: url(../assets/Navigation/bg-item-2x.png);
+}
+.PathItem .link .item {
+	bottom:0;
+	left:0;
+}
+.metaicondetail {
+	background:#fff;
+	color:#fff;
+	border-radius:6px;
+	border:1px #333 solid;
+	min-width:100px;
+	max-width:300px;
+	overflow:hidden;
+	text-align: center;
+	position:absolute;
+	display:none;
+	top:-115px;
+	left:-22px;
+}
+.metaicondetail .inner {
+	border:2px #fff solid;
+	border-radius:6px;
+	background:#272727;
+	font-size:14px;
+	padding:5px;
+}
+.metaicondetail span {
+	font-family:candara, arail;
+	font-size:18px;
+}
+.metaicondetail s {
+	border-color:#333 transparent transparent;
+	border-style: solid dashed dashed;
+	border-width:5px;
+	clear: both;
+	display: inline-block;
+	font-size: 0;
+	height: 0;
+	margin-left:3px;
+	overflow: hidden;
+	position: absolute;
+	right:50%;
+	top:44px;
+	width:0;
 }
 </style>
--- src/components/Biography.vue
+++ src/components/Biography.vue
@@ -1,8 +1,29 @@
 <template>
+  <div class="wrapper">
+    <p class="learn">
+      This is a demo, <a href="https://paulrhayes.com/2009-04/auto-scrolling-parallax-effect-without-javascript/">learn how it works</a>.
+    </p>
+    
+    <p class="learn">hahh</p>
 
+    <div id="experiment">
+      <div id="background"></div>
+      <div id="content">
+        <h2 class="learn">Mouse over to begin animation</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+    
+  </div>
 </template>
 
 <script>
+$(function(){
+  $(".learn").click(function(){
+    alert(1)
+  })
+}
+)
 export default {
   name: 'Biography',
   data() {
@@ -24,22 +45,42 @@ export default {
   text-align: center;
   padding-top: 100px !important;
 }
-.example{background:#FFF;width:730px;border:1px #000 solid;margin:20px auto;padding:15px;-moz-border-radius: 3px;-webkit-border-radius: 3px}
 
-#water {
-    width:500px;
-    height:400px;
-    display: block;
-    margin:0px auto;
-    cursor:pointer;
+#background {
+    background: url('../assets/foreground.png') 5% 5%, url('../assets/midground.png') 50% 50%, url('../assets/background.png') 90% 110%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    position: absolute;
+    -webkit-transition: left 300s linear;
+    -moz-transition: left 300s linear;
+    -o-transition: left 300s linear;
+    -ms-transition: left 300s linear;
+    transition: left 300s linear;
 }
-#switcher {
-    text-align:center;
-    overflow:hidden;
-    margin:15px;
+
+#experiment:target #background {
+    left: -5000px;
 }
-#switcher img {
-    width:160px;
-    height:120px;
+
+#experiment:hover #background {
+    left: -9999px;
+}
+
+#content {
+    margin: 6em 10em;
+    background-color: #333;
+    opacity: 0.95;
+    color: #ccc;
+    padding: 3em;
+    border: 2px solid #666;
+}
+
+#content h2 {
+    margin: 1em 0;
+    font-weight: bold;
+    font-size:  26px;
+    color: #fff;
 }
 </style>
